9461e07308816deab0e7c3091626099838c2678c
Kestrel96 <kuba.poltorak@gmail.com>
2022-11-08 23:42:54 +0100

    Final version of kill script (I hope)wq

--- kill.sh
+++ kill.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 pid="$(ps aux | grep cut | grep -vw "grep" | awk '{print $2}')"
 
-echo "Killing CUT, PID: $pid"
-kill $pid
+if [ "$pid" == "" ]
+then
+	echo "Did not find CUT running..."
+	exit 0
+else
 
+	echo "Killing CUT, PID: $pid"
+	kill $pid
+fi
